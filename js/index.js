77193ebd51750e98be36b1abacb38be90ae43095
Gabriel dos Santos <gsantos.4dev@gmail.com>
2022-06-13 21:46:16 -0300

    enter

--- js/index.js
+++ js/index.js
@@ -14,3 +14,7 @@ function calc(){
         document.getElementById('resultado').innerHTML = 'Digite valores!'
     }
 }
+function back(){
+    var resultado = document.getElementById('resultado').innerHTML 
+    document.getElementById('resultado').innerHTML  = resultado.substring(0, resultado.length - 1)
+}
