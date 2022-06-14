e4b1ef5900467fbc2785ee3a6537122a562383e0
Gabriel dos Santos <gsantos.4dev@gmail.com>
2022-06-14 10:32:13 -0300

    enter

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
+// colando os elementos na tag <p>
 function insert(num){
     var numero = document.getElementById('resultado').innerHTML
     document.getElementById('resultado').innerHTML = numero + num
 }
+// limpando os elementos da tag <p>
 function clean(){
     document.getElementById('resultado').innerHTML = ""
 }
+// Mostra o resultado da equação quando se é clicado no botão "="
 function calc(){
     var resultado = document.getElementById('resultado').innerHTML
     if(resultado){
@@ -14,6 +17,7 @@ function calc(){
         document.getElementById('resultado').innerHTML = 'Digite valores!'
     }
 }
+// tirando o ultimo caractere da tag <p>
 function back(){
     var resultado = document.getElementById('resultado').innerHTML 
     document.getElementById('resultado').innerHTML  = resultado.substring(0, resultado.length - 1)
